6d7e22510a57a908a3fa8352df50edee0ddc6237
Sunil Joshi <sunil.joshi@airfi.com>
2023-04-17 16:58:51 +0530

    fixed

--- tools/build.sh
+++ tools/build.sh
@@ -1,6 +1,7 @@
 prepareBuildFolder() {
   # Copy project directories to build folder
 #   cd $BUILD_TMP_DIR
+  cd $PROJECT_DIR
   echo "Copying client and captive portal in build directory" >> test.txt
   ls -l $PROJECT_DIR
   cp -r $PROJECT_DIR/test.txt $BUILD_TMP_DIR
