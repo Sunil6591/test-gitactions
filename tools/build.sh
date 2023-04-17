fa5fae348e059b6a6fd1f7b3dfd68b1968068a1f
Sunil Joshi <sunil.joshi@airfi.com>
2023-04-17 16:47:07 +0530

    fixed

--- tools/build.sh
+++ tools/build.sh
@@ -1,13 +1,15 @@
 prepareBuildFolder() {
   # Copy project directories to build folder
-  cd $BUILD_TMP_DIR
+#   cd $BUILD_TMP_DIR
   echo "Copying client and captive portal in build directory" >> test.txt
-  ls -l
+  ls -l $PROJECT_DIR
+  cp -r $PROJECT_DIR/test.txt $BUILD_TMP_DIR
+  ls -l $BUILD_TMP_DIR
 }
 
 finishBuild() {
   echo "Creating build archive ..."
-#   cd $BUILD_TMP_DIR
+  cd $BUILD_TMP_DIR
   pwd
   zip --quiet --recurse-paths test.zip *
   pwd
@@ -18,8 +20,9 @@ finishBuild() {
 # ALERTC='\033[33;5;7m'
 # SUCCESSC='\033[0;32m'
 # NC='\033[0m'
-# mkdir ../artifacts
-BUILD_TMP_DIR=$(mktemp -d)
+mkdir ../artifacts
+BUILD_TMP_DIR=artifacts
+PROJECT_DIR=$(dirname $PWD)
 pwd
 
 prepareBuildFolder
